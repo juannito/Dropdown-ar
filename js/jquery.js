368a154c5e3965dfc3486c06e26f7bd7693e9d90
Juannito <juannito@gmail.com>
2014-09-11 22:15:54 -0300

    Prepend

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-
-    console.log("Hola estoy funcionando")
+    
+    // OCULTAR EL BLOQUE
+    $("ul").hide();
+    
+    $("i").click(function(){ // FUNCIONA ANONIMA. Se usa en esta porcion de código. Atada a este evento.
+        $("ul").slideToggle();
+        console.log("Yeah!");
+    }); 
+    
+    $(".link").append(" <i class='fa fa-check'></i>"); // te posiciona algo detras del elemeneto. PREPEND es para que te lo ponga antes.
 
 });
